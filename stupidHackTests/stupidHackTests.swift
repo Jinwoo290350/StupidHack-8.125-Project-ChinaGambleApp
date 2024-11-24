231d619641237052dc3ea9a4aa03ed51e112778d
FRANK <frankhupu@gmail.com>
2024-11-24 09:50:11 +0700

    Add files via upload

---
+++ stupidHackTests/stupidHackTests.swift
@@ -0,0 +1,17 @@
+//
+//  stupidHackTests.swift
+//  stupidHackTests
+//
+//  Created by Veerapong Haphuriwat on 22/11/2567 BE.
+//
+
+import Testing
+@testable import stupidHack
+
+struct stupidHackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
